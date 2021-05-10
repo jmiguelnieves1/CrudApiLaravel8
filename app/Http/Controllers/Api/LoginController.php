738e674a879b0de73fe5c8647e5b3f050bd4f177
Jose desde Evoo <jmiguelnieves1@gmail.com>
2021-05-10 15:58:03 -0400

    Lo demas

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+
+    private function validateLogin(Request $request){
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+    }
+
+    public function login(Request $request){
+        //Validamos
+        $this->validateLogin($request);
+
+        if(Auth::attempt($request->only('email', 'password'))){
+            return response()->json([
+                //Creamos el token del usuario y el name es el dispositivo del que se coencta. Por ejemplo IphoneToken
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
+            ]);
+        }else{
+            return response()->json([
+                'message' => 'No autorizado',
+            ], 401);
+        }
+    }
 }
--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -7,6 +7,8 @@ use App\Http\Resources\V2\PostCollection;
 use App\Http\Resources\V2\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -19,7 +21,21 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|min:5|max:254',
+            'content' => 'required|min:10',
+        ]);
+
+        $post = new Post();
+        $post->user_id = Auth::user()->id;
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->title);
+        $post->content = $request->content;
+        $post->save();
+
+        return response()->json([
+            new PostResource($post)
+        ], 201);
     }
 
     public function show(Post $post)
@@ -29,7 +45,18 @@ class PostController extends Controller
 
     public function update(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            'title' => 'required|min:5|max:254',
+            'content' => 'required|min:10',
+        ]);
+
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->update();
+
+        return response()->json([
+            new PostResource($post)
+        ], 202);
     }
 
     public function destroy(Post $post)
